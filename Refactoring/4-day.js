6907cdb8d8cc798e2b60ebd34d20758f384233bf
Kabadzh0b <chubbudda@gmail.com>
2023-01-04 18:29:44 +0200

    Create 4-day.js
    Replace initialization of i into for instead of global context of function.

---
+++ Refactoring/4-day.js
@@ -0,0 +1,19 @@
+// Get day number
+
+// Step 3
+// Replace initialization of i into for instead of global context of function.
+
+"use strict";
+
+const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const parseDay = (s) => {
+  for (let i = 0; i < Days.length; i++) {
+    if (s.startsWith(Days[i].toLowerCase())) {
+      return i + 1;
+    }
+  }
+  return -1;
+};
+
+require("../Tests/day.js")(parseDay);
