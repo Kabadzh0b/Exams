991ac0683ee88cafa3d4e053ea1ceff7b86e26cf
Kabadzh0b <chubbudda@gmail.com>
2023-01-04 19:26:27 +0200

    Create 7-day.js
    used eslint

---
+++ Refactoring/7-day.js
@@ -0,0 +1,20 @@
+// Get day number
+
+// Step 6
+// Modify array 'Days' to lowercase.
+// Delete .toLowerCase()
+
+'use strict';
+
+const Days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+const parseDay = (str) => {
+  for (let i = 0; i < Days.length; i++) {
+    if (str.startsWith(Days[i])) {
+      return i + 1;
+    }
+  }
+  return -1;
+};
+
+require('../Tests/day.js')(parseDay);
