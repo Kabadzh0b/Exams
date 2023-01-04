32884fa0ea7e815ee7e8ddbea22e1890a4a05735
Kabadzh0b <chubbudda@gmail.com>
2023-01-04 19:27:54 +0200

    Create 3-month.js
    used Eslint

---
+++ Refactoring/3-month.js
@@ -0,0 +1,30 @@
+// Get month number
+
+// Step 2
+// Rename 's' on 'month'
+
+'use strict';
+
+const Months = [
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'may',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'oct',
+  'nov',
+  'dec',
+];
+
+const Month = (month) => {
+  for (let i = 0; i < Months.length; i++) {
+    if (month.toLowerCase().startsWith(Months[i])) return i + 1;
+  }
+  return -1;
+};
+
+require('../Tests/month.js')(Month);
