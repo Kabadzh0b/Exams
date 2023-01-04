376d7b08b069283c66c3e59adf53df8abcd136d8
Kabadzh0b <chubbudda@gmail.com>
2023-01-04 18:22:17 +0200

    'D' is never reassigned. Use 'const' instead

--- Refactoring/2-day.js
+++ Refactoring/2-day.js
@@ -1,12 +1,11 @@
 // Get day number
 
-// Step 1
-// Add 'use strict';
-// '_parse_day_' is not defined
+// Step 2
+// 'D' is never reassigned. Use 'const' instead
 
 "use strict";
 
-let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const _parse_day_ = (s) => {
   let i;
